5334dc26162e6ada64cf196f3ef50e809efc9b99
Anchit Kalra <ak007kalra@gmail.com>
2021-01-15 12:50:40 +0530

    post.html complete

--- scripts/post.js
+++ scripts/post.js
@@ -6,6 +6,29 @@ var post_1 = 'Since JavaScript does not have any type-checking,'+
 
  var name1 = "Srishti Gupta";
 
+
+ var post_2 = 'One of the biggest challenges in software development is time. It’s something we can’t easily get more of, but linting can help us make the most out of the time we have.';
+var name2 = 'Colby Fayock';
+var title2 = 'What is linting and how can it save you time?';
+
+
+var post_3 = "If you're a developer with a Twitter account, you've already seen everyone and their cat start a blog, YouTube channel, or Patreon.We all want to become stars, or at the very least, a recognizable name in the industry.";
+var name3 = 'Yazeed Bzadough';
+var title3 = 'How to Get More Views on Your Tech Blog';
+
+var post_4 = 'When code is not describable using words, most people have to do some mental mapping to turn it in to words. This wastes mental energy, and you run the risk of getting the mapping wrong. Different people will map to different words, which'+
+'leads to confusion when discussing the code.';
+var name4 = 'Cedd Burge';
+var title4 = 'How to write easily describable code';
+
+
+var post_5 = 'Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node, and you can say that you’ve created a module '+
+'named xyz.';
+var name5 = 'Srishti Gupta';
+var title5 = 'Everything you should know about ‘module’ & ‘require’ in Node.js';
+
+
+
  var i = -1;
 
 showPost();
@@ -22,8 +45,52 @@ function showPost() {
                         var c = document.createElement('p');
                         c.innerHTML = post_1;
                         b.appendChild(c);
-                 
+                
+                      
             }
+            else if(a[1] == 2) {
+                document.getElementById('heading1').innerHTML = title2;
+                var name = document.getElementById('headingName');
+                name.innerHTML += name2;
+                name.innerHTML += '<button id = "edit" class = "btn btn-danger"style = "margin-left : 90%;" onclick = "editPost()">EDIT <i class="fa fa-edit"></i> </button>';
+                var b = document.getElementById('post1Text');
+                var c = document.createElement('p');
+                c.innerHTML = post_2;
+                b.appendChild(c);
+            } 
+
+            else if(a[1] == 5) {
+                document.getElementById('heading1').innerHTML = title5;
+                var name = document.getElementById('headingName');
+                name.innerHTML += name5;
+                name.innerHTML += '<button id = "edit" class = "btn btn-danger"style = "margin-left : 90%;" onclick = "editPost()">EDIT <i class="fa fa-edit"></i> </button>';
+                var b = document.getElementById('post1Text');
+                var c = document.createElement('p');
+                c.innerHTML = post_5;
+                b.appendChild(c);
+            } 
+
+            else if(a[1] == 3) {
+                document.getElementById('heading1').innerHTML = title3;
+                var name = document.getElementById('headingName');
+                name.innerHTML += name3;
+                name.innerHTML += '<button id = "edit" class = "btn btn-danger"style = "margin-left : 90%;" onclick = "editPost()">EDIT <i class="fa fa-edit"></i> </button>';
+                var b = document.getElementById('post1Text');
+                var c = document.createElement('p');
+                c.innerHTML = post_3;
+                b.appendChild(c);
+            } 
+
+            else if(a[1] == 4) {
+                document.getElementById('heading1').innerHTML = title4;
+                var name = document.getElementById('headingName');
+                name.innerHTML += name4;
+                name.innerHTML += '<button id = "edit" class = "btn btn-danger"style = "margin-left : 90%;" onclick = "editPost()">EDIT <i class="fa fa-edit"></i> </button>';
+                var b = document.getElementById('post1Text');
+                var c = document.createElement('p');
+                c.innerHTML = post_4;
+                b.appendChild(c);
+            } 
     }
 
     function editPost() {
@@ -43,14 +110,14 @@ function showPost() {
                 b.setAttribute('type', 'text');
                 a.replaceWith(b);
                 b.setAttribute('style', 'width : 100%; margin-left : 30%');
-                b.value = title1;
+                b.value = a.innerText;
 
                 var c = document.getElementById('post1Text');
                 var b = document.createElement('textArea');
                 b.setAttribute('id', 'post');
                 c.replaceWith(b);
                 b.setAttribute('style', 'width : 100%;');
-                b.value = post_1;       
+                b.value = c.innerText;       
                 
     }
 
@@ -94,7 +161,6 @@ function displayCount() {
 function displayComment(id) {
         var a = document.getElementById(id);
         var b = document.createElement('p');
-        b.setAttribute('style', 'margin-bottom : 10px;');
         var c =  document.getElementById('0');
         b.innerHTML = c.innerHTML;
         c.innerHTML = a.value;
